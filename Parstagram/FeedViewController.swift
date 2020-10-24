f204b82841c5073ed75c5de09359736e232eca5c
grigori <gbarbachov@bktaxcpa.com>
2020-10-23 20:11:41 -0700

    Bonus 1

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -12,6 +12,8 @@ import AlamofireImage
 class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var posts = [PFObject]()
+    var refreshControl: UIRefreshControl!
+
 
     @IBOutlet weak var tableView: UITableView!
     
@@ -21,6 +23,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         tableView.dataSource = self
         
         // Do any additional setup after loading the view.
+        refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(onRefresh), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0)
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -37,10 +43,29 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // Implement the delay method
+        func run(after wait: TimeInterval, closure: @escaping () -> Void) {
+            let queue = DispatchQueue.main
+            queue.asyncAfter(deadline: DispatchTime.now() + wait, execute: closure)
+        }
         
-        return posts.count
-    }
+        // Call the delay method in your onRefresh() method
+        func refresh() {
+            run(after: 2) {
+                self.refreshControl.endRefreshing()
+            }
+        }
+        @objc func onRefresh() {
+            run(after: 0) {
+                self.refreshControl.endRefreshing()
+            }
+            refresh()
+        }
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return posts.count
+        }
+        
+  
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
