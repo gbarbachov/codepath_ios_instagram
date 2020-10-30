1f794c109a9f67aa85a4522f992bc3764a1d0abc
grigori <gbarbachov@bktaxcpa.com>
2020-10-29 22:03:19 -0700

    Logging out

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -15,8 +15,18 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     var refreshControl: UIRefreshControl!
 
 
+        
     @IBOutlet weak var tableView: UITableView!
     
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+              let delegate = windowScene.delegate as? SceneDelegate
+        else {return}
+        delegate.window?.rootViewController = loginViewController
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
