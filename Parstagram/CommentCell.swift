d9a9465cc46c66197e9c9673329160f96ec05671
grigori <gbarbachov@bktaxcpa.com>
2020-10-29 23:09:29 -0700

    Started CommentCell

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Grigori on 10/29/20.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
